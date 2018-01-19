50000744fc0eb33beeae4a186d7782dc4b8b853e
marcomilanesio <milanesio.marco@gmail.com>
2018-01-19 14:25:06 +0100

    cleaning first test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,13 +2,17 @@ import unittest
 import numpy as np
 import utils
 
+
 class TestUtilsMethods(unittest.TestCase):
 
+    def setUp(self):
+        size = 500
+        self.x = np.linspace(0, 1000, size)
+
     def test_standardize(self):
-        x = np.linspace(0, 1000, 500)
-        standardized = utils.standardize(x)
-        assert np.allclose(np.mean(standardized), 0)
-        assert np.allclose(np.std(standardized), 1)
+        stdx = utils.standardize(self.x)
+        assert np.allclose(np.mean(stdx), 0)
+        assert np.allclose(np.std(stdx), 1)
 
 
 if __name__ == '__main__':
