6c53550147d68abd4f26187a431c0bf2d5e29585
marcomilanesio <milanesio.marco@gmail.com>
2018-01-19 16:49:58 +0100

    EOD; new branch

--- main.py
+++ main.py
@@ -5,35 +5,62 @@ from torch.autograd import Variable
 from models import linmodel
 import utils
 
-Nsamples = 400  # TO SCALE
-Xold = np.linspace(0, 1000, Nsamples).reshape([Nsamples, 1])
-X = utils.standardize(Xold)
+from pyspark import SparkContext
 
-W = np.random.randint(1, 10, size=(5, 1))
+def prepare_input(nsamples=400):
+    Xold = np.linspace(0, 1000, nsamples).reshape([nsamples, 1])
+    X = utils.standardize(Xold)
 
-Y = W.dot(X.T)  # target
+    W = np.random.randint(1, 10, size=(5, 1))
 
-for i in range(Y.shape[1]):
-    Y[:, i] = utils.add_noise(Y[:, i])
+    Y = W.dot(X.T)  # target
 
-x = Variable(torch.from_numpy(X), requires_grad=False).type(torch.FloatTensor)
-y = Variable(torch.from_numpy(Y), requires_grad=False).type(torch.FloatTensor)
+    for i in range(Y.shape[1]):
+        Y[:, i] = utils.add_noise(Y[:, i])
 
-# how to split x, y
+    x = Variable(torch.from_numpy(X), requires_grad=False).type(torch.FloatTensor)
+    y = Variable(torch.from_numpy(Y), requires_grad=False).type(torch.FloatTensor)
+    return x, y, W
 
-model = linmodel.LinModel(1, 5)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+def instantiate_model(x):
+    model = linmodel.LinModel(1, 5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+    prediction = model(x)
 
-for i in range(5000):
-    prediction = model(x)  # x = (400, 1): x1 = (200. 1). x2 = (200, 1)
-    loss = linmodel.cost(y, prediction)
-    optimizer.zero_grad()
-    loss.backward()  # get the gradients
+    return model, optimizer, prediction
 
-    # sum gradients
 
-    optimizer.step()  #
+def step(rdd1, rdd2):
+    rdd = rdd1.map(instantiate_model)
 
-print([param.data for param in model.parameters()])
-print(W)
+    return None
+
+    model = linmodel.LinModel(1, 5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
+
+    for i in range(5000):
+        prediction = model(v1)  # x = (400, 1): x1 = (200. 1). x2 = (200, 1)
+        loss = linmodel.cost(v2, prediction)
+        optimizer.zero_grad()
+        loss.backward()  # get the gradients
+
+        # sum gradients
+
+        optimizer.step()  #
+    return model
+
+
+def main(sc, num_partitions=4):
+    x, y, W = prepare_input()
+    rdd_x = sc.parallelize(x).repartition(num_partitions)
+    rdd_y = sc.parallelize(y).repartition(num_partitions)
+    model = step(rdd_x, rdd_y)
+    exit(0)
+    return model, W
+
+if __name__ == '__main__':
+    sc = SparkContext(appName='pyspato')
+    model, W = main(sc)
+    print([param.data for param in model.parameters()])
+    print(W)
