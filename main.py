5afbf2ddce235bb7361d72f99b395a61e3741ca5
marcomilanesio <milanesio.marco@gmail.com>
2018-02-19 15:48:00 +0100

    mse calculation on results

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ def init_data(nsamples=400):
 
     x = Variable(torch.from_numpy(X), requires_grad=True).type(torch.FloatTensor)
     y = Variable(torch.from_numpy(Y), requires_grad=True).type(torch.FloatTensor)
-    return x, y, W
+    w = Variable(torch.from_numpy(W), requires_grad=True).type(torch.FloatTensor)
+    return x, y, w
 
 
 def instantiate_model(x, y):
@@ -34,16 +35,24 @@ def step(x, y, model, optimizer):
     loss = linmodel.cost(y, prediction)
     optimizer.zero_grad()
     loss.backward()  # get the gradients
-    print([param.grad.data for param in model.parameters()])
+    # print([param.grad.data for param in model.parameters()])
     # sum gradients
     optimizer.step()  #
     return model, optimizer
 
 if __name__ == "__main__":
-    x, y, W = init_data()
+    x, y, w = init_data()
     m, o = instantiate_model(x, y)
     for i in range(5000):
         m, o = step(x, y, m, o)
 
-    print([param.data for param in m.parameters()])
-    print(W)
+    r = np.array([param.data for param in m.parameters()])
+    res = Variable(r[0])
+
+    # print(w.size(), res.size())
+    # print(type(w), type(res))
+    # print(w, res)
+    #
+    # m = torch.sum((w - res)**2)
+    # print(m)
+    print(linmodel.mse(res, w))
