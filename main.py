606e17b8d1c3e2ad1533bfd19adae1f4c1e4678d
marcomilanesio <milanesio.marco@gmail.com>
2018-02-21 16:30:03 +0100

    update gradient on split partitions

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from torch.autograd import Variable
 
 from models import linmodel
 import utils
+import threading
 
-NUM_ITERATIONS = 10000
+NUM_ITERATIONS = 1500
 
-N = 1000   # 50 - 500 - 1000 - 5000
+N = 400   # 50 - 500 - 1000 - 5000
 dx = 1   # log fino a 1M (0-6)
 dy = 5
 
@@ -29,15 +30,15 @@ def prepare_input(nsamples=N):
     return x, y, W
 
 
-def save_model_state(model):
-    model_dict = model.state_dict()
-    try:
-        gradients = [param.grad.data for param in model.parameters()]
-    except AttributeError:
-        gradients = None
-    state = {'model': model_dict,
-             'gradients': gradients}
-    return state
+# def save_model_state(model):
+#     model_dict = model.state_dict()
+#     try:
+#         gradients = [param.grad.data for param in model.parameters()]
+#     except AttributeError:
+#         gradients = None
+#     state = {'model': model_dict,
+#              'gradients': gradients}
+#     return state
 
 
 def create_model(insize, outsize):
@@ -61,16 +62,46 @@ def local_step(x, y, model):
     return model
 
 
+def update_model(model, g):
+    model.linear.weight.grad = g
+    return model
+
+
+def run(model, x, y):
+    model = local_step(x, y, model)
+    local_grad = Variable([param.grad.data for param in model.parameters()][0])
+    return model, local_grad
+
+
 def main(num_partitions=4):
     x, y, W = prepare_input()
 
-    # parts_x = list(torch.split(x, int(x.size()[0] / num_partitions)))
-    # parts_y = list(torch.split(y, int(x.size()[0] / num_partitions), 1))
+    parts_x = list(torch.split(x, int(x.size()[0] / num_partitions)))
+    parts_y = list(torch.split(y, int(x.size()[0] / num_partitions), 1))
 
+    q = [(i, j) for i, j in zip(parts_x, parts_y)]
     model = create_model(dx, dy)
 
-    for i in range(NUM_ITERATIONS):
-        model = local_step(x, y, model)
+    x1, y1 = q[0]
+    x2, y2 = q[1]
+    x3, y3 = q[2]
+    x4, y4 = q[3]
+    for _ in range(250):
+        tmp = []
+        model1, t1 = run(model, x1, y1)
+        tmp.append(t1)
+
+        model2, t2 = run(model, x2, y2)
+        tmp.append(t2)
+
+        model3, t3 = run(model, x3, y3)
+        tmp.append(t3)
+
+        model4, t4 = run(model, x4, y4)
+        tmp.append(t4)
+
+        s = gradients_sum(tmp)
+        model = update_model(model1, s)
 
     return model, W
 
