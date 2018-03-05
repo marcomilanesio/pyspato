b8e6e624d3b086294c0e3d43f7733e99ff2a75ca
marcomilanesio <milanesio.marco@gmail.com>
2018-03-05 16:56:34 +0100

    forcing W to be invertible

--- main.py
+++ main.py
@@ -4,18 +4,25 @@ from torch.autograd import Variable
 
 from models import linmodel
 import utils
+import numpy.linalg as linalg
+import sys
 
 
 def init_data(nsamples, dx, dy):
     Xold = np.linspace(0, 1000, nsamples * dx).reshape([nsamples, dx])
     X = utils.standardize(Xold)
 
-    W = np.random.randint(1, 10, size=(dy, dx))
+    invertible = False
+    while not invertible:
+        W = np.random.randint(1, 10, size=(dy, dx))
+        if linalg.cond(W) < 1 / sys.float_info.epsilon:
+            invertible = True
+            print('W invertible')
 
     Y = W.dot(X.T)  # target
 
-    for i in range(Y.shape[1]):
-        Y[:, i] = utils.add_noise(Y[:, i])
+    # for i in range(Y.shape[1]):
+    #     Y[:, i] = utils.add_noise(Y[:, i])
 
     x = Variable(torch.from_numpy(X), requires_grad=True).type(torch.FloatTensor)
     y = Variable(torch.from_numpy(Y), requires_grad=True).type(torch.FloatTensor)
@@ -37,7 +44,7 @@ def step(x, y, model, optimizer):
     # print([param.grad.data for param in model.parameters()])
     # sum gradients
     optimizer.step()  #
-    return model, optimizer
+    return model, optimizer, loss.data.numpy()
 
 
 def get_mse(m, w):
@@ -46,15 +53,29 @@ def get_mse(m, w):
     return linmodel.mse(res, w)
 
 if __name__ == "__main__":
-    NUM_ITERATIONS = 600
+    NUM_ITERATIONS = 10000
 
-    N = 50  # 50 - 500 - 1000 - 5000
-    dx = 10  # log fino a 1M (0-6)
+    N = 500  # 50 - 500 - 1000 - 5000
+    dx = 1  # log fino a 1M (0-6)
     dy = 5
 
     x, y, w = init_data(N, dx, dy)
-    m, o = instantiate_model(dx, dy)
-    for i in range(NUM_ITERATIONS):
-        m, o = step(x, y, m, o)
 
-    print(get_mse(m, w))
+    m, o = instantiate_model(dx, dy)
+
+    losses = []
+    for i in range(NUM_ITERATIONS):
+        m, o, l = step(x, y, m, o)
+        losses.append(l)
+
+    print(min(losses), max(losses))
+    import matplotlib.pyplot as plt
+    plt.plot(losses)
+    plt.ylim(0, 10000)
+    plt.show()
+    # print(get_mse(m, w))
+    print(w)
+    print([param.data for param in m.parameters()])
+    pred = m(x)
+    plt.scatter(pred.data.numpy(), y.data.numpy())
+    plt.show()
