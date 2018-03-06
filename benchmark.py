86d578793ac59edc0306ad1652dad789eb5320da
marcomilanesio <milanesio.marco@gmail.com>
2018-03-06 15:22:09 +0100

    benchmark w/plots

--- benchmark.py
+++ benchmark.py
@@ -1,10 +1,9 @@
 import numpy as np
 import main
 import time
-import csv
+import matplotlib.pyplot as plt
 
-fname = './fixed_n_iterations.csv'
-fieldnames = ['n_iterations', 'dx', 'mse', 'time']
+# fieldnames = ['n_iterations', 'dx', 'mse', 'time']
 
 n_iterations = 5000
 n_samples = [50, 500, 1000, 5000]
@@ -13,21 +12,37 @@ dy = 5
 
 results = []
 
+
+def plot_loss(losses, lossfname):
+    fig, ax = plt.subplots()
+    ax.plot(losses)
+    # plt.ylim(0, 10000)
+    # plt.xlim(0, 10000)
+    fig.savefig(lossfname)
+    plt.close()
+
+
+def plot_scatter(model, in_, out_, scatterfname):
+    pred = model(in_)
+    fig, ax = plt.subplots()
+    ax.scatter(pred.data.numpy(), out_.data.numpy())
+    fig.savefig(scatterfname)
+    plt.close()
+
+
 for N in [50, 500, 1000, 5000]:
-    for dx in [1, 10, 100, 1000, 10000, 100000, 1000000]:
+    for dx in [1, 10, 100, 1000, 10000, 100000]:
+        lossfname = "./plots/loss-{}-{}.png".format(N, dx)
+        scatterfname = "./plots/scatter-{}-{}.png".format(N, dx)
         print("running ", N, dx)
         start = time.time()
         x, y, w = main.init_data(N, dx, dy)
         m, o = main.instantiate_model(dx, dy)
+        losses = []
         for i in range(n_iterations):
-            m, o = main.step(x, y, m, o)
+            m, o, l = main.step(x, y, m, o)
+            losses.append(l)
 
-        results.append((N, dx, main.get_mse(m, w), time.time() - start))
+        plot_loss(losses, lossfname)
+        plot_scatter(m, x, y, scatterfname)
 
-print('Saving results...')
-with open(fname, 'w') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(fieldnames)
-    for row in results:
-        writer.writerow(row)
-print('Done')
