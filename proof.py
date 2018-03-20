22ae748867ef45e38f897bfd479068235e273f67
marcomilanesio <milanesio.marco@gmail.com>
2018-03-20 10:43:03 +0100

    proof file for theory

---
+++ proof.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+y = np.random.randn(100).reshape(100,1)
+x = np.random.randn(100*10).reshape((100,10))
+w = np.random.randn(10).reshape([10,1])
+print(y.shape, x.shape, w.shape)
+
+E = np.sum((y - x.dot(w))**2)
+print(E)
+grad = -2 * x.T.dot(y - x.dot(w))
+print(grad)
+
+y1 = y[50:,]
+y2 = y[:50,]
+x1 = x[50:,:]
+x2 = x[:50,:]
+
+#print(y1.shape, x1.shape, w.shape)
+grad1 = -2 * x1.T.dot(y1 - x1.dot(w))
+grad2 = -2 * x2.T.dot(y2 - x2.dot(w))
+print(grad1)
+print(grad2)
+print((grad1 + grad2) - grad)
