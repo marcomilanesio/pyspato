03f256749883865fca4972925ad3e2dffa52a5dd
marcomilanesio <milanesio.marco@gmail.com>
2018-03-21 11:15:06 +0100

    define function for init_data and ground_truth

--- proof.py
+++ proof.py
@@ -1,23 +1,53 @@
 import numpy as np
+import sys
 
-y = np.random.randn(100).reshape(100,1)
-x = np.random.randn(100*10).reshape((100,10))
-w = np.random.randn(10).reshape([10,1])
-print(y.shape, x.shape, w.shape)
 
-E = np.sum((y - x.dot(w))**2)
-print(E)
-grad = -2 * x.T.dot(y - x.dot(w))
-print(grad)
+def check_if_invertible(m):
+    return np.linalg.cond(m) < 1 / sys.float_info.epsilon
 
-y1 = y[50:,]
-y2 = y[:50,]
-x1 = x[50:,:]
-x2 = x[:50,:]
 
-#print(y1.shape, x1.shape, w.shape)
-grad1 = -2 * x1.T.dot(y1 - x1.dot(w))
-grad2 = -2 * x2.T.dot(y2 - x2.dot(w))
-print(grad1)
-print(grad2)
-print(all((grad1 + grad2) - grad) < 1e-12)
+def init_data(nsamples, dx, dy=1):
+    x = np.random.randn(nsamples * dx).reshape((nsamples, dx))
+    w = None
+    y = None
+
+    invertible = False
+    while not invertible:
+        w = np.random.randn(dx).reshape([dx, dy])
+        invertible = check_if_invertible(w)
+
+    if w:
+        y = x.dot(w)
+    else:
+        exit('oops!')
+
+    assert y.shape == (nsamples, dy)
+
+    print(y.shape, x.shape, w.shape)
+    return x, y, w
+
+
+def closed_form(x, y, w, splits=2):
+    E = np.sum((y - x.dot(w))**2)
+    print(E)
+    grad = -2 * x.T.dot(y - x.dot(w))
+    print(grad)
+
+    slice_dim = y.shape[0] / splits
+    y1 = y[50:,]
+    y2 = y[:50,]
+    x1 = x[50:,:]
+    x2 = x[:50,:]
+
+    #print(y1.shape, x1.shape, w.shape)
+    grad1 = -2 * x1.T.dot(y1 - x1.dot(w))
+    grad2 = -2 * x2.T.dot(y2 - x2.dot(w))
+    print(grad1)
+    print(grad2)
+    print(all((grad1 + grad2) - grad) < 1e-12)
+
+
+if __name__ == '__main__':
+    n = 100
+    dx = 10
+    x, y, w = init_data(n, dx)
