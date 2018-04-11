43ea2610182ef0c2afb51cba8325054e6fbf1000
marcomilanesio <milanesio.marco@gmail.com>
2018-04-11 10:58:33 +0200

    Signed-off-by: marcomilanesio <milanesio.marco@gmail.com>

--- multi.py
+++ multi.py
@@ -43,24 +43,6 @@ def step(x, y, model, optimizer, g=None):
     return model, optimizer, loss.data.numpy()
 
 
-# def run(x, y, m, o):
-#     name = threading.current_thread().name
-#     print('{} working on {}-{}'.format(name, x.size(), y.size()))
-#     # print('{} working on {}'.format(threading.current_thread().name, [param.grad.data for param in m.parameters()]))
-#     tmp = []
-#     for i in range(2500):
-#         m, o, l = step(x, y, m, o)
-#         tmp.append(l)
-#     return m, name, tmp
-
-
-def run_single(x, y, m, o, new_gradient):
-    name = multiprocessing.current_process().name
-    m, o, l = step(x, y, m, o, new_gradient)
-    g = Variable([param.grad.data for param in m.parameters()][0])
-    return m, name, l, g
-
-
 def gradients_sum(gradients):
     # print('gradients: {}'.format(gradients))
     s = torch.stack(gradients)
@@ -68,78 +50,100 @@ def gradients_sum(gradients):
     return su
 
 
-def run_on_queue(parts_q, model, gradients_q, losses_q, new_gradient):
-    o = torch.optim.Adam(model.parameters(), lr=1e-2)
-    x, y = parts_q.get()
-    m, name, l, g = run_single(x, y, model, o, new_gradient)
-    # gradients_q.put(g)
-    losses_q.put(l)
-    parts_q.put((x, y))
-    # print('rq', multiprocessing.current_process().name, g)
-    # evt.wait()
+# def run_on_queue(parts_q, model, gradients_q, losses_q, new_gradient):
+#     o = torch.optim.Adam(model.parameters(), lr=1e-2)
+#     x, y = parts_q.get()
+#     m, name, l, g = run_single(x, y, model, o, new_gradient)
+#     # gradients_q.put(g)
+#     losses_q.put(l)
+#     parts_q.put((x, y))
+#     # print('rq', multiprocessing.current_process().name, g)
+#     # evt.wait()
 
-def run_on_queue2(parts_q, model):
-    o = torch.optim.Adam(model.parameters(), lr=1e-2)
-    x, y = parts_q
-    new_gradient = None
-    m, name, l, g = run_single(x, y, model, o, new_gradient)
-    return l
-    # gradients_q.put(g)
-    # losses_q.put(l)
-    # parts_q.put((x, y))
-    # print('rq', multiprocessing.current_process().name, g)
-    # evt.wait()
-
-
-def run_monolithic(x, y, w, dx, dy, NUM_ITERATIONS):
-    m, o = instantiate_model(dx, dy)
-    t0 = time.time()
-    # monolithic run
-    losses_mono = []
-    for i in range(NUM_ITERATIONS):
-        m, o, l = step(x, y, m, o)
-        losses_mono.append(l)
-    # fig, ax = plt.subplots()
-    # ax.plot(losses_mono)
-    # plt.ylim(0, 10000)
-    # plt.xlim(0, NUM_ITERATIONS)
-    # fig.savefig('./plots/monolithic.png')
-    t1 = time.time()
-    print('monolithic run done in {} msec'.format("%.2f" % (1000 * (t1 - t0))))
-    estimated = [param.data for param in m.parameters()]
-    return losses_mono, estimated
+# def run_on_queue2(parts_q, model):
+#     o = torch.optim.Adam(model.parameters(), lr=1e-2)
+#     x, y = parts_q
+#     new_gradient = None
+#     m, name, l, g = run_single(x, y, model, o, new_gradient)
+#     return l
+#     # gradients_q.put(g)
+#     # losses_q.put(l)
+#     # parts_q.put((x, y))
+#     # print('rq', multiprocessing.current_process().name, g)
+#     # evt.wait()
 
 
-def run(parts, model, queue_g, new_g):
+def grad_closed_form(x, y, w):
+    xn = x.data.numpy()
+    yn = y.data.numpy()
+    wn = w.numpy().T
+    print('Yn', yn.shape, 'Xn', xn.shape, 'Wn', wn.shape)
+    grad = -2 * xn.T.dot(yn - xn.dot(wn))
+    return grad
+
+
+def run(parts, model, optimizer, queue_g, new_g):
+    name = multiprocessing.current_process().name
     x, y = parts
-    g = multi_step(x, y, model, new_g)
+    m, o, l = step(x, y, model, optimizer, new_g)
+    g = Variable([param.grad.data for param in m.parameters()][0])
+    w_temp = [param.data for param in m.parameters()][0]
+    close_grad = grad_closed_form(x, y, w_temp)
+    print('in run method of {}'.format(name))
+    print(close_grad)
+    print(g)
+    print('--')
     queue_g.put(g)
     return x.size()
 
 
-def multi_step(x, y, model, new_g):
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
-    m, name, l, g = run_single(x, y, model, optimizer, new_g)
-    return g
+def plot_loss(loss, fname=False):
+    fig, ax = plt.subplots()
+    ax.plot(loss)
+    # plt.ylim(0, 10000)
+    plt.xlim(0, NUM_ITERATIONS)
+    if fname:
+        fig.savefig(fname)
+    else:
+        plt.show()
+
+
+def monolithic_run(x, y, m, o, num_iterations):
+    t0 = time.time()
+    losses = []
+    for i in range(num_iterations):
+        m, o, l = step(x, y, m, o)
+        # print('mono param', [param.data for param in m.parameters()])
+        # print('mono grad', [param.grad.data for param in m.parameters()])
+        losses.append(l)
+
+    t1 = time.time()
+    print('monolithic run done in {} msec'.format("%.2f" % (1000 * (t1 - t0))))
+    estimated = [param.data for param in m.parameters()]
+    return losses, estimated
 
 if __name__ == "__main__":
 
-    NUM_ITERATIONS = 2
+    NUM_ITERATIONS = 5000
     NUM_PARTITIONS = 2
-    N = 100  # 50 - 500 - 1000 - 5000
+    N = 500  # 50 - 500 - 1000 - 5000
     dx = 10  # log fino a 1M (0-6)
     dy = 1
 
     x, y, w = init_data(N, dx, dy)
-    # mono_loss, mono_estimated = run_monolithic(x, y, w, dx, dy, NUM_ITERATIONS)
-
+    m, o = instantiate_model(dx, dy)
+    losses, mono_params = monolithic_run(x, y, m, o, NUM_ITERATIONS)
+    # plot_loss(losses, False)
+    print(w)
+    print(mono_params)
+    exit()
     mp.set_start_method('spawn')
     model, optimizer = instantiate_model(dx, dy)
 
     parts_x = list(torch.split(x, int(x.size()[0] / NUM_PARTITIONS)))
     parts_y = list(torch.split(y, int(y.size()[0] / NUM_PARTITIONS)))
     parts = [(i, j) for i, j in zip(parts_x, parts_y)]
-
+    print('number of splits = {}'.format(len(parts)))
     model.share_memory()
     mngr = mp.Manager()
 
@@ -151,14 +155,16 @@ if __name__ == "__main__":
     new_gradient = None
 
     with mp.Pool(processes=num_processes) as pool:
-        p = partial(run, model=model, queue_g=gradients_q, new_g=new_gradient)
-        res = pool.map(p, parts)
-        local_gradients = []
-        while not gradients_q.empty():
-            local_gradients.append(gradients_q.get())
+        for i in range(NUM_ITERATIONS):
+            p = partial(run, model=model, optimizer=optimizer, queue_g=gradients_q, new_g=new_gradient)
+            res = pool.map(p, parts)
+            local_gradients = []
+            while not gradients_q.empty():
+                local_gradients.append(gradients_q.get())
 
-        new_gradient = gradients_sum(local_gradients)
-        print(new_gradient)
+            new_gradient = gradients_sum(local_gradients)
+            print('pool', new_gradient)
+
 
         # for i in range(NUM_ITERATIONS):
         #     losses = []
