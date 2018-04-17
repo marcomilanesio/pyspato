43a26731ce37fc0efb4dbeb9a18a0f5b1233d3b1
marcomilanesio <milanesio.marco@gmail.com>
2018-04-17 16:09:09 +0200

    cleaning

--- multi.py
+++ multi.py
@@ -25,22 +25,8 @@ def init_data(nsamples, dx, dy):
 
 def instantiate_model(dx, dy):
     model = linmodel.LinModel(dx, dy)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-2)
     return model, optimizer
-
-
-def step(x, y, model, optimizer, g=None):
-    prediction = model(x)  # x = (400, 1): x1 = (200. 1). x2 = (200, 1)
-    loss = linmodel.cost(y, prediction)
-    optimizer.zero_grad()
-    loss.backward()  # get the gradients
-    # if g is not None:
-    #     model.linear.weight.grad = g
-    # print([param.grad.data for param in model.parameters()])
-    # sum gradients
-    optimizer.step()  #
-    # print('loss', loss)
-    return model, optimizer, loss.data.numpy()
 
 
 def gradients_sum(gradients):
@@ -48,29 +34,6 @@ def gradients_sum(gradients):
     s = torch.stack(gradients)
     su = torch.sum(s, dim=0)
     return su
-
-
-# def run_on_queue(parts_q, model, gradients_q, losses_q, new_gradient):
-#     o = torch.optim.Adam(model.parameters(), lr=1e-2)
-#     x, y = parts_q.get()
-#     m, name, l, g = run_single(x, y, model, o, new_gradient)
-#     # gradients_q.put(g)
-#     losses_q.put(l)
-#     parts_q.put((x, y))
-#     # print('rq', multiprocessing.current_process().name, g)
-#     # evt.wait()
-
-# def run_on_queue2(parts_q, model):
-#     o = torch.optim.Adam(model.parameters(), lr=1e-2)
-#     x, y = parts_q
-#     new_gradient = None
-#     m, name, l, g = run_single(x, y, model, o, new_gradient)
-#     return l
-#     # gradients_q.put(g)
-#     # losses_q.put(l)
-#     # parts_q.put((x, y))
-#     # print('rq', multiprocessing.current_process().name, g)
-#     # evt.wait()
 
 
 def run(parts, model, optimizer):
@@ -125,7 +88,7 @@ if __name__ == "__main__":
     NUM_PARTITIONS = 2
     N = 500  # 50 - 500 - 1000 - 5000
     dx = 5  # log fino a 1M (0-6)
-    dy = 1
+    dy = 5
 
     # torch variables
     x, y, w = init_data(N, dx, dy)
@@ -133,12 +96,8 @@ if __name__ == "__main__":
     losses, mono_params, m = monolithic_run(x, y, model, optimizer, NUM_ITERATIONS)
     plot_loss(losses, False)
 
-
     print(w)
-    # print([param.data for param in m.parameters()])
     print(m.state_dict())
-    # exit()
-
 
     mp.set_start_method('spawn')
     model, optimizer = instantiate_model(dx, dy)
@@ -153,9 +112,6 @@ if __name__ == "__main__":
 
     num_processes = NUM_PARTITIONS
 
-    gradients_q = mngr.Queue(maxsize=num_processes)
-
-    # evt = mp.Event()
     new_gradient = None
 
     with mp.Pool(processes=num_processes) as pool:
@@ -168,52 +124,18 @@ if __name__ == "__main__":
             optimizer.step()
 
     print(model.state_dict())
-    exit()
 
-    # for i in range(NUM_ITERATIONS):
-    #     processes = []
-    #     for pid in range(num_processes):
-    #         p = mp.Process(target=run_on_queue, args=(parts_q, m, gradients_q, losses_q, new_gradient))
-    #         p.start()
-    #         processes.append(p)
-    #
-    #     for p in processes:
-    #         p.join()
-    #
-    #     local_gradients = []
-    #     while not gradients_q.empty():
-    #         # print("Got:", gradients_q.get())
-    #         local_gradients.append(gradients_q.get())
-    #
-    #     new_gradient = gradients_sum(local_gradients)
-    #     # print('new:', new_gradient)
-    #
-    #     local_losses = []
-    #     while not losses_q.empty():
-    #         got = losses_q.get()
-    #         # print('got', got)
-    #         local_losses.append(got)
-    #
-    #     #print(i, local_losses)
-    #     print(i, sum(local_losses)[0], l_mono[i][0])
-
-    exit()
-
-
-
-
-
-    fig, axes = plt.subplots(5, 2, sharex=True, sharey=True)
-    i, j = 0, 0
-    for name, lst in losses.items():
-        axes[i % 5, j % 2].plot(lst, label='{}'.format(name))
-        legend = axes[i % 5, j % 2].legend(loc='upper right')
-        plt.legend()
-        plt.ylim(0, 3000)
-        plt.xlim(0, 500)
-        i += 1
-        j += 1
-    fig.savefig('./plots/process_loss.png')
+    # fig, axes = plt.subplots(5, 2, sharex=True, sharey=True)
+    # i, j = 0, 0
+    # for name, lst in losses.items():
+    #     axes[i % 5, j % 2].plot(lst, label='{}'.format(name))
+    #     legend = axes[i % 5, j % 2].legend(loc='upper right')
+    #     plt.legend()
+    #     plt.ylim(0, 3000)
+    #     plt.xlim(0, 500)
+    #     i += 1
+    #     j += 1
+    # fig.savefig('./plots/process_loss.png')
     # for k, v in losses.items():
     #     print(k, v[-2:])
 
