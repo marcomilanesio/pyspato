7e6d84985aa0e8f0952c4ade0e3d50338043c6b3
marcomilanesio <milanesio.marco@gmail.com>
2018-04-17 13:50:06 +0200

    added plots

--- benchmark.py
+++ benchmark.py
@@ -7,11 +7,13 @@ n_iterations = 2500
 
 fieldnames = ['dx', 'dy', 'msec', 'error']
 
-def main(N, dx, dy):
+
+def main(N, dx, dy, plotfname):
     t0 = time.time()
     x, y, w = multi.init_data(N, dx, dy)
     model, optimizer = multi.instantiate_model(dx, dy)
     losses, mono_params, m = multi.monolithic_run(x, y, model, optimizer, n_iterations)
+    multi.plot_loss(losses, plotfname)
     westimated = [p.data for p in m.parameters()][0]
     t1 = time.time()
     tres = (t1 - t0) * 1000
@@ -19,12 +21,13 @@ def main(N, dx, dy):
 
 if __name__ == "__main__":
     for N in [50, 500, 1000, 5000]:
-        fname = '{}.csv'.format(N)
+        fname = './results/{}.csv'.format(N)
         res = []
 
         for dx in map(int, np.power(10, range(0, 7))):
             for dy in map(int, np.power(10, range(0, 7))):
-                t, we, x, y = main(N, dx, dy)
+                plotfname = './results/{}-{}-{}.png'.format(N, dx, dy)
+                t, we, x, y = main(N, dx, dy, plotfname)
                 err = np.sum((we.dot(x.T) - y) ** 2)
                 res.append({'dx': dx, 'dy': dy, 'msec': t, 'error': err})
                 print(N, dx, dy, t, err)
